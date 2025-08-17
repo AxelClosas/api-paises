1dc79f85be2e2c424a3c95394fb80ea7035c78d8
Axel Closas <axelclosas13@gmail.com>
2025-08-17 05:04:54 -0300

    feat: corregir esquema de modelo de País para capital y fronteras

--- src/api/models/Pais.mjs
+++ src/api/models/Pais.mjs
@@ -2,11 +2,11 @@ import mongoose from "mongoose"
 
 const paisSchema = new mongoose.Schema({
   nombreOficial: { type: String, minlength: 3, maxlength: 90, required: true },
-  capital: { type: String, minlength: 3, maxlength: 90, required: true },
+  capital: { type: [String], min: 1, minlength: 3, maxlength: 90, required: true },
   continente: { type: String, minlength: 3, maxlength: 90, required: true },
   subContinente: { type: String, minlength: 3, maxlength: 90, required: true },
   idiomas: { type: Map, of: String },
-  fronteras: { type: [String], maxlength: 3 },
+  fronteras: { type: [String], minlength:3, maxlength: 3 },
   area: { type: Number, min: 0, max: 20000000 },
   mapas: { googleMaps: String, openStreetMaps: String },
   poblacion: { type: Number, min: 0 },
@@ -14,9 +14,9 @@ const paisSchema = new mongoose.Schema({
   zonasHorarias: [String],
   banderas: { type: Map, of: String, required: true },
   creadoPor: { type: String, required: true },
+  nombreApi: { type: String, default: 'api-paises' },
   fechaCreacion: { type: Date, default: Date.now }
 })
 
-const Pais = mongoose.Model('Pais', paisSchema, 'Grupo-04')
-
+const Pais = mongoose.model('Pais', paisSchema, 'Grupo-04')
 export default Pais
