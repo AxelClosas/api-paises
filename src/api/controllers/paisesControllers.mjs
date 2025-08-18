fb1e299647f9796d965fe30716bddeaca7398c63
Axel Closas <axelclosas13@gmail.com>
2025-08-18 12:55:04 -0300

    Primeros pasos en la implementación del controlador del procesoGuardarPaisesDesdeAPIOriginalEnMongoDB

---
+++ src/api/controllers/paisesControllers.mjs
@@ -0,0 +1,9 @@
+import { procesoGuardarPaisesDesdeAPIOriginalEnMongoDB } from '../services/paisesServices.mjs'
+
+export async function procesoGuardarPaisesDesdeAPIOriginalEnMongoDBController(req, res) {
+  try {
+    
+  } catch (error) {
+
+  }
+}
