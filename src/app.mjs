bf88aa355fcaea9a5ed6355beb8cffa60bef9462
Axel Closas <axelclosas13@gmail.com>
2025-08-18 12:54:32 -0300

    Se movio de lugar el punto de entrada de la aplicación.
    antes: api-paises/app.mjs
    
    ahora: api-paises/src/app.mjs

---
+++ src/app.mjs
@@ -0,0 +1,51 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { conectarBD } from './api/config/dbConfig.mjs'
+import path from 'path'
+import expressLayouts from 'express-ejs-layouts'
+import paisesRoutes from './api/routes/paisesRoutes.mjs'
+// Se importa la configuración de nuestra variable de entorno
+dotenv.config()
+
+// Se instancia la aplicación express
+const app = express()
+
+// Se define el puerto desde la variable de entorno, caso contrario se utiliza el puerto 3001
+const PORT = process.env.PORT || 3001
+
+// Middleware para trabajar con JSON
+app.use(express.json())
+
+// Middleware para trabajar con formularios HTML
+app.use(express.urlencoded({ extended: true }))
+
+/* Middleware para servir archivos estáticos */
+app.use(express.static(path.resolve('./src/public')))
+
+// Configuramos la ruta para acceder a la api
+app.use('/api', paisesRoutes)
+
+// Se configura EJS como Motor de Vistas
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./src/views'))
+console.log(path.resolve('./src/views'))
+
+// Se configura expressLayouts
+app.use(expressLayouts)
+app.set('layout', 'layout')
+
+
+
+
+// Conexión a la Base de Datos
+conectarBD()
+
+
+app.get('/', (req, res) => {
+  res.status(200).render('index', { title: 'Página Principal'})
+})
+
+app.listen(PORT, () => {
+  console.log("API PAISES")
+  console.log(`http://localhost:${PORT}`)
+})
