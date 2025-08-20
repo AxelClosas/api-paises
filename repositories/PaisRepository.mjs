49808b84251d9c0aee0cb5401e558d55ceb92bf2
Axel Closas <axelclosas13@gmail.com>
2025-08-20 00:43:24 -0300

    Se eliminaron metodos obtenerTodos y obtenerPorAtributo

--- repositories/PaisRepository.mjs
+++ repositories/PaisRepository.mjs
@@ -5,10 +5,6 @@ class PaisRepository extends IRepository {
   async obtenerPorId(id) {
     return await Pais.findById(id)
   }
-  // De momento no le encuentro utilidad para la aplicación actual
-  // async obtenerTodos() {
-  //   return await Pais.find({})
-  // }
 
   async agregar(pais) {
     return await Pais.create(pais)
@@ -16,10 +12,6 @@ class PaisRepository extends IRepository {
 
   async actualizar(id, atributos) {
     return await Pais.findByIdAndUpdate(id, atributos, { new: true })
-  }
-
-  async buscarPorAtributo(atributo, valor) {
-    return await Pais.find({ [atributo]: valor })
   }
 
   async buscarPorQuery(query) {
