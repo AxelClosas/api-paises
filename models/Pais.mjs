b178ed2c2e6b4b916fa648a405d09d8d19e34df7
Axel Closas <axelclosas13@gmail.com>
2025-08-28 00:45:09 -0300

    Requerir campos en el esquema de país: idiomas, área, mapas, población, gini, zonas horarias y nombreApi

--- models/Pais.mjs
+++ models/Pais.mjs
@@ -5,16 +5,16 @@ const paisSchema = new mongoose.Schema({
   capital: { type: [String], min: 1, minlength: 3, maxlength: 90, required: true },
   continente: { type: String, minlength: 3, maxlength: 90, required: true },
   subContinente: { type: String, minlength: 3, maxlength: 90, required: true },
-  idiomas: [String],
+  idiomas: { type:[String], required: true },
   fronteras: { type: [String], minlength:3, maxlength: 3 },
-  area: { type: Number, min: 0, max: 20000000 },
-  mapas: { googleMaps: String, openStreetMaps: String },
-  poblacion: { type: Number, min: 0 },
-  gini: { type: Map, of: Number },
-  zonasHorarias: [String],
+  area: { type: Number, min: 0, max: 20000000, required: true },
+  mapas: { googleMaps: String, openStreetMaps: String, required: true },
+  poblacion: { type: Number, min: 0, required: true },
+  gini: { type: Map, of: Number, required: true},
+  zonasHorarias: {type: [String], required: true},
   banderas: { type: Map, of: String, required: true },
   creadoPor: { type: String, required: true },
-  nombreApi: { type: String, default: 'api-paises' },
+  nombreApi: { type: String, default: 'api-paises', required: true },
   fechaCreacion: { type: Date, default: Date.now }
 })
 
